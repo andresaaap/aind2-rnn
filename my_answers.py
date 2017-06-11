197e50671b546f9425d3862c4df48874cfde8247
Medi Innovation <companycentralinfo@gmail.com>
2017-06-11 10:18:09 -0500

    Update the clean text function to reduce the number of valid characters

--- my_answers.py
+++ my_answers.py
@@ -68,7 +68,7 @@ def build_part1_RNN(step_size, window_size):
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     test_text = text
-    valid_english_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', ',', '.', ':', ';', '?']
+    valid_english_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', ',', '.', ':', ';', '?']
     for i in range(0,len(valid_english_characters)):
         #Leave in the test_text only characters that are not valid english characters.
         test_text = test_text.replace(valid_english_characters[i],'')
@@ -77,14 +77,13 @@ def clean_text(text):
     #Print all non-valid english characters to check if there is any character that shouldn't be a  non-valid english character
     print(nonvalid_english_characters)
     #Based on the characters analysis the following define the final non-valid english characters
-    nonvalid_english_characters = ['/', '(', '"', '&', '%', '*', ')', '-', '$', '@', "'"]
+    nonvalid_english_characters = ['/', '(', '"', '&', '%', '*', ')', '-', '$', '@', "'", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'è', 'é',  'à', 'â']
     #Remove non-valid english characters from text 
     for i in range(0,len(nonvalid_english_characters)):
         text = text.replace(nonvalid_english_characters[i],'')
-
-    
+        
     # shorten any extra dead space created above
-    text = text.replace('  ',' ') 
+    text = text.replace('  ',' ')
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
